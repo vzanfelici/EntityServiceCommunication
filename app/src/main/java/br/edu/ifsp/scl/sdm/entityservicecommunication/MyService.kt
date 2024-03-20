b803ac0a80709c29ed94e3acbd63c56b15737fd2
Vinícius Zanfelici <86609742+vinizaan@users.noreply.github.com>
2024-03-20 13:21:32 -0300

    initial commit

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MyService.kt
@@ -0,0 +1,12 @@
+package br.edu.ifsp.scl.sdm.entityservicecommunication
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
