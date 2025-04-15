6b6ea02428d477097164a73f9f24bd6f2ab429d4
Vinícius Zanfelici <86609742+vinizaan@users.noreply.github.com>
2025-04-14 21:04:47 -0300

    part: 1

--- app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sdm/entityservicecommunication/MainActivity.kt
@@ -1,28 +1,41 @@
 package br.edu.ifsp.scl.sdm.entityservicecommunication
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import br.edu.ifsp.scl.sdm.entityservicecommunication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private lateinit var incrementServiceIntent: Intent
+    private var counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
-        with (amb) {
+        incrementServiceIntent = Intent(this, IncrementService::class.java)
+        with(amb) {
             mainTb.apply {
                 getString(R.string.app_name).also { setTitle(it) }
                 setSupportActionBar(this)
             }
             incrementBt.setOnClickListener {
-
+                startService(incrementServiceIntent.apply {
+                    putExtra("VALUE", counter)
+                })
             }
         }
+        InterEntityCommunication.valueLiveData.observe(this) { value ->
+            counter = value
+            Toast.makeText(this, "You clicked $counter times.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopService(incrementServiceIntent)
     }
 }
